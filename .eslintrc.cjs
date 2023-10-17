bfda4172cfc5f59e53f4c4817dd43f13b4f0bfa1
Masaya Nakamura <mashabow@shiromoji.net>
2023-10-17 23:21:28 +0900

    test: introduce typescript-eslint
    https://typescript-eslint.io/linting/typed-linting
    https://typescript-eslint.io/linting/troubleshooting#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,21 @@
 /** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
   root: true,
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
   reportUnusedDisableDirectives: true,
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['./**/*.cjs'],
+    },
+  ],
 };
