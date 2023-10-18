13784e980594635793c2eed63a99018d881a7649
Masaya Nakamura <mashabow@shiromoji.net>
2023-10-18 23:57:16 +0900

    refactor: use `Object.entries`

--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,19 @@ const $methods = [
 function createMockFromApiStructure<T extends ApiStructure>(
   apiStructure: T,
 ): MockApi<T> {
-  const keys = Object.keys(apiStructure) as (keyof T)[];
-
-  return keys.reduce((acc, key) => {
+  return Object.entries(apiStructure).reduce((acc, [key, value]) => {
     if (key === '$path') {
-      return { ...acc, $path: apiStructure.$path };
+      return { ...acc, $path: value };
     }
 
-    if (apiStructure[key] instanceof Function) {
+    if (value instanceof Function) {
       // メソッド or パスパラメータ
       // TODO: メソッドのモックを作る or パスパラメータ以下のモックを再帰的に作る
       return acc;
     }
 
     // サブパスのモックを再帰的に作る
-    return { ...acc, [key]: createMockFromApiStructure(apiStructure[key]) };
+    return { ...acc, [key]: createMockFromApiStructure(value) };
   }, {} as MockApi<T>);
 }
 
