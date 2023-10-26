4e1f206f6d63f46ae8f50eefee50531d8bec609d
Masaya Nakamura <mashabow@shiromoji.net>
2023-10-27 01:26:24 +0900

    fix: fix usage example

--- example/index.ts
+++ example/index.ts
@@ -3,5 +3,10 @@ import { mswpida } from '../src';
 
 const mock = mswpida(api, 'https://example.com');
 
-mock.pet._petId.$post();
-mock.user.createWithList.$post();
+mock.pet._petId.$post(async (req, res, ctx) =>
+  res(ctx.status(201), ctx.json(await req.json())),
+);
+
+mock.user.createWithList.$post(async (req, res, ctx) =>
+  res(ctx.status(201), ctx.json(await req.json())),
+);
