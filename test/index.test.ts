14427faaaa6df98efeb134af233710a85fc7747e
Masaya Nakamura <mashabow@shiromoji.net>
2023-10-30 22:43:14 +0900

    test: reorganize

--- test/index.test.ts
+++ test/index.test.ts
@@ -32,103 +32,76 @@ describe('createTypedRest', () => {
     });
   });
 
-  describe('エンドポイントのパスを `.$path()` で取得できる', () => {
+  describe('作成された `typedRest` が正しく動く', () => {
     const defaultBaseURL = 'http://petstore.swagger.io/api'; // Pet Store API のデフォルトの base URL
     const customBaseURL = 'https://custom-base-url.example.com/v1';
 
-    test('パスパラメータを含まない場合は、具体的なパスをそのまま返す', () => {
-      const typedRest = createTypedRest(petStoreApi);
-      expect(typedRest.pets.$path()).toEqual(`${defaultBaseURL}/pets`);
-    });
-
-    test('パスパラメータ部分は `:foo` 形式で表現される', () => {
-      const typedRest = createTypedRest(petStoreApi);
-      expect(typedRest.pets._id.$path()).toEqual(`${defaultBaseURL}/pets/:id`);
-    });
-
-    test('`createTypedRest` で `baseURL` オプションを指定した場合、それがベース URL として使われる', () => {
-      const typedRest = createTypedRest(petStoreApi, {
-        baseURL: customBaseURL,
-      });
-      expect(typedRest.pets._id.$path()).toEqual(`${customBaseURL}/pets/:id`);
-    });
-  });
-
-  describe('request handler を `.$get()` などで作成できる', () => {
-    const server = setupServer();
-    beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-    afterEach(() => server.resetHandlers());
-    afterAll(() => server.close());
-
-    test('作成した request handler が正しく動く', async () => {
-      const typedRest = createTypedRest(petStoreApi);
-      server.use(
-        typedRest.pets._id.$get((req, res, ctx) => {
-          const petId = req.params.id;
-          return res(
-            ctx.status(200),
-            ctx.json({ id: Number(petId), name: 'Foo' }),
-          );
-        }),
-      );
-
-      const client = petStoreApi(aspida(fetch));
-      const resBody = await client.pets._id(123).$get();
-
-      expect(resBody).toEqual({ id: 123, name: 'Foo' });
-    });
-
-    test('`createTypedRest` で `baseURL` オプションを指定した場合、それがベース URL として使われる', async () => {
-      const customBaseURL = 'https://custom-base-url.example.com/v1';
-      const typedRest = createTypedRest(petStoreApi, {
-        baseURL: customBaseURL,
+    describe('エンドポイントのパスを `.$path()` で取得できる', () => {
+      test('パスパラメータを含まない場合は、具体的なパスをそのまま返す', () => {
+        const typedRest = createTypedRest(petStoreApi);
+        expect(typedRest.pets.$path()).toEqual(`${defaultBaseURL}/pets`);
       });
 
-      server.use(
-        typedRest.pets._id.$get((req, res, ctx) => {
-          const petId = req.params.id;
-          return res(
-            ctx.status(200),
-            ctx.json({ id: Number(petId), name: 'Foo' }),
-          );
-        }),
-      );
-
-      const client = petStoreApi(aspida(fetch, { baseURL: customBaseURL }));
-      const resBody = await client.pets._id(123).$get();
-      expect(resBody).toEqual({ id: 123, name: 'Foo' });
-    });
-  });
-
-  test('`req.params` にパスパラメータの型がつく', () => {
-    const api = (({ baseURL: _baseURL }) => ({
-      items: {
-        _itemId: (itemId: string) => ({
-          $get: () => Promise.resolve({ foo: 'bar' }),
-          $path: () => `${_baseURL}/items/${itemId}`,
-          variants: {
-            $get: () => Promise.resolve({ foo: 'bar' }),
-            $path: () => `${_baseURL}/items/${itemId}/variants`,
-            _variantId: (variantId: number) => ({
-              $get: () => Promise.resolve({ foo: 'bar' }),
-              $path: () => `${_baseURL}/items/${itemId}/variants/${variantId}`,
-            }),
-          },
-        }),
-      },
-    })) satisfies Api;
-    const typedRest = createTypedRest(api);
-    const handler = typedRest.items._itemId.variants._variantId.$get(
-      (req, res, ctx) => {
-        const itemId = req.params.itemId satisfies string;
-        const variantId = req.params.variantId satisfies string;
-        return res(
-          ctx.status(200),
-          ctx.json({ foo: 'baz', itemId, variantId }),
+      test('パスパラメータ部分は `:foo` 形式で表現される', () => {
+        const typedRest = createTypedRest(petStoreApi);
+        expect(typedRest.pets._id.$path()).toEqual(
+          `${defaultBaseURL}/pets/:id`,
         );
-      },
-    );
+      });
 
-    expect(handler).toBeInstanceOf(RestHandler);
+      test('`createTypedRest` で `baseURL` オプションを指定した場合、それがベース URL として使われる', () => {
+        const typedRest = createTypedRest(petStoreApi, {
+          baseURL: customBaseURL,
+        });
+        expect(typedRest.pets._id.$path()).toEqual(`${customBaseURL}/pets/:id`);
+      });
+    });
+
+    describe('request handler を `.$get()` などで作成できる', () => {
+      const server = setupServer();
+      beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+      afterEach(() => server.resetHandlers());
+      afterAll(() => server.close());
+
+      test('作成した request handler が正しく動く', async () => {
+        const typedRest = createTypedRest(petStoreApi);
+        server.use(
+          typedRest.pets._id.$get((req, res, ctx) => {
+            const petId = req.params.id;
+            return res(
+              ctx.status(200),
+              ctx.json({ id: Number(petId), name: 'Foo' }),
+            );
+          }),
+        );
+
+        const client = petStoreApi(aspida(fetch));
+        const resBody = await client.pets._id(123).$get();
+
+        expect(resBody).toEqual({ id: 123, name: 'Foo' });
+      });
+
+      test('`createTypedRest` で `baseURL` オプションを指定した場合、それがベース URL として使われる', async () => {
+        const typedRest = createTypedRest(petStoreApi, {
+          baseURL: customBaseURL,
+        });
+
+        server.use(
+          typedRest.pets._id.$get((req, res, ctx) => {
+            const petId = req.params.id;
+            return res(
+              ctx.status(200),
+              ctx.json({ id: Number(petId), name: 'Foo' }),
+            );
+          }),
+        );
+
+        const client = petStoreApi(aspida(fetch, { baseURL: customBaseURL }));
+        const resBody = await client.pets._id(123).$get();
+        expect(resBody).toEqual({ id: 123, name: 'Foo' });
+      });
+    });
+
+    // TODO: 型のテストを追加する
   });
 });
