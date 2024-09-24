26af778b1f5ccecc6bad4925b50484c00d39909f
Masaya Nakamura <mashabow@shiromoji.net>
2024-09-24 22:55:14 +0900

    refactor: $ pnpm biome format --write

--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,93 @@
-import { RequestHandlerOptions, ResponseResolver, http } from 'msw';
-import { LowerHttpMethod } from 'aspida';
+import { RequestHandlerOptions, ResponseResolver, http } from "msw";
+import { LowerHttpMethod } from "aspida";
 import {
-  $LowerHttpMethod,
-  ApiInstance,
-  Api,
-  Endpoint,
-  TypedHttp,
-} from './type';
+	$LowerHttpMethod,
+	ApiInstance,
+	Api,
+	Endpoint,
+	TypedHttp,
+} from "./type";
 
 const METHODS = [
-  'get',
-  'post',
-  'put',
-  'delete',
-  'head',
-  'patch',
-  'options',
+	"get",
+	"post",
+	"put",
+	"delete",
+	"head",
+	"patch",
+	"options",
 ] satisfies LowerHttpMethod[];
 const $METHODS = [
-  '$get',
-  '$post',
-  '$put',
-  '$delete',
-  '$head',
-  '$patch',
-  '$options',
+	"$get",
+	"$post",
+	"$put",
+	"$delete",
+	"$head",
+	"$patch",
+	"$options",
 ] satisfies $LowerHttpMethod[];
 
 function createTypedHttpFromApiInstance<
-  TApiInstance extends ApiInstance,
-  TPathParamName extends string,
+	TApiInstance extends ApiInstance,
+	TPathParamName extends string,
 >(apiInstance: TApiInstance): TypedHttp<TApiInstance, TPathParamName> {
-  // @ts-expect-error TODO: 型エラー修正
-  return Object.entries(apiInstance).reduce(
-    // @ts-expect-error TODO: 型エラー修正
-    (acc, [key, value]) => {
-      if (value instanceof Function) {
-        if (key === '$path') {
-          return { ...acc, $path: value };
-        }
+	// @ts-expect-error TODO: 型エラー修正
+	return Object.entries(apiInstance).reduce(
+		// @ts-expect-error TODO: 型エラー修正
+		(acc, [key, value]) => {
+			if (value instanceof Function) {
+				if (key === "$path") {
+					return { ...acc, $path: value };
+				}
 
-        if ($METHODS.includes(key as $LowerHttpMethod)) {
-          // そのメソッドのモック生成関数を返す
-          const method = key.substring(1) as LowerHttpMethod;
-          const path = (apiInstance as Endpoint).$path();
-          return {
-            ...acc,
-            [key]: (
-              resolver: ResponseResolver,
-              options?: RequestHandlerOptions,
-            ) => http[method](path, resolver, options),
-          };
-        }
+				if ($METHODS.includes(key as $LowerHttpMethod)) {
+					// そのメソッドのモック生成関数を返す
+					const method = key.substring(1) as LowerHttpMethod;
+					const path = (apiInstance as Endpoint).$path();
+					return {
+						...acc,
+						[key]: (
+							resolver: ResponseResolver,
+							options?: RequestHandlerOptions,
+						) => http[method](path, resolver, options),
+					};
+				}
 
-        if (METHODS.includes(key as LowerHttpMethod)) {
-          // `$` 無しのメソッドは、モックには用意しない
-          return acc;
-        }
+				if (METHODS.includes(key as LowerHttpMethod)) {
+					// `$` 無しのメソッドは、モックには用意しない
+					return acc;
+				}
 
-        if (key.startsWith('_')) {
-          // 次の階層がパスパラメータ（e.g. `_foo`）の場合、パスを MSW の形式（`:foo`）に変換した上で、再帰的にモックを作る
-          const paramName = key.substring(1);
-          // @ts-expect-error TODO: 型エラー修正
-          const childApiInstance = value(`:${paramName}`) as ApiInstance;
-          return {
-            ...acc,
-            [key]: createTypedHttpFromApiInstance(childApiInstance),
-          };
-        }
+				if (key.startsWith("_")) {
+					// 次の階層がパスパラメータ（e.g. `_foo`）の場合、パスを MSW の形式（`:foo`）に変換した上で、再帰的にモックを作る
+					const paramName = key.substring(1);
+					// @ts-expect-error TODO: 型エラー修正
+					const childApiInstance = value(`:${paramName}`) as ApiInstance;
+					return {
+						...acc,
+						[key]: createTypedHttpFromApiInstance(childApiInstance),
+					};
+				}
 
-        return acc; // ここには来ないはず
-      }
+				return acc; // ここには来ないはず
+			}
 
-      // サブパスのモックを再帰的に作る
-      return { ...acc, [key]: createTypedHttpFromApiInstance(value) };
-    },
-    {} as TypedHttp<TApiInstance, TPathParamName>,
-  );
+			// サブパスのモックを再帰的に作る
+			return { ...acc, [key]: createTypedHttpFromApiInstance(value) };
+		},
+		{} as TypedHttp<TApiInstance, TPathParamName>,
+	);
 }
 
 export function createTypedHttp<TApiInstance extends ApiInstance>(
-  api: Api<TApiInstance>,
-  options?: { baseURL?: string },
+	api: Api<TApiInstance>,
+	options?: { baseURL?: string },
 ): TypedHttp<TApiInstance, never> {
-  const apiInstance = api({
-    baseURL: options?.baseURL,
-    // @ts-expect-error 使わないので適当な関数を渡しておく
-    fetch: () => 'dummy',
-  });
+	const apiInstance = api({
+		baseURL: options?.baseURL,
+		// @ts-expect-error 使わないので適当な関数を渡しておく
+		fetch: () => "dummy",
+	});
 
-  return createTypedHttpFromApiInstance(apiInstance);
+	return createTypedHttpFromApiInstance(apiInstance);
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,16 @@
-import aspida from '@aspida/fetch';
-import { setupServer } from 'msw/node';
-import { afterAll, afterEach, beforeAll, describe, expect, test } from 'vitest';
-import { HttpResponse } from 'msw';
-import { createTypedHttp } from '../src';
-import { Api } from '../src/type';
-import petStoreApi from './generated-api/$api';
+import aspida from "@aspida/fetch";
+import { setupServer } from "msw/node";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
+import { HttpResponse } from "msw";
+import { createTypedHttp } from "../src";
+import { Api } from "../src/type";
+import petStoreApi from "./generated-api/$api";
 
-describe('createTypedHttp', () => {
-  describe('API の構造に沿った `typedHttp` が作成される', () => {
-    test('openapi2aspida で生成した Pet Store API から、Pet Store API 用の `typedHttp` が作成される', () => {
-      const typedHttp = createTypedHttp(petStoreApi);
-      expect(typedHttp).toMatchInlineSnapshot(`
+describe("createTypedHttp", () => {
+	describe("API の構造に沿った `typedHttp` が作成される", () => {
+		test("openapi2aspida で生成した Pet Store API から、Pet Store API 用の `typedHttp` が作成される", () => {
+			const typedHttp = createTypedHttp(petStoreApi);
+			expect(typedHttp).toMatchInlineSnapshot(`
         {
           "pets": {
             "$get": [Function],
@@ -24,101 +24,101 @@ describe('createTypedHttp', () => {
           },
         }
       `);
-    });
+		});
 
-    test('空の API からは空の `typedHttp` が作成される', () => {
-      const emptyApi = (() => ({})) satisfies Api;
-      const typedHttp = createTypedHttp(emptyApi);
-      expect(typedHttp).toEqual({});
-    });
-  });
+		test("空の API からは空の `typedHttp` が作成される", () => {
+			const emptyApi = (() => ({})) satisfies Api;
+			const typedHttp = createTypedHttp(emptyApi);
+			expect(typedHttp).toEqual({});
+		});
+	});
 
-  describe('作成された `typedHttp` が正しく動く', () => {
-    const defaultBaseURL = 'http://petstore.swagger.io/api'; // Pet Store API のデフォルトの base URL
-    const customBaseURL = 'https://custom-base-url.example.com/v1';
+	describe("作成された `typedHttp` が正しく動く", () => {
+		const defaultBaseURL = "http://petstore.swagger.io/api"; // Pet Store API のデフォルトの base URL
+		const customBaseURL = "https://custom-base-url.example.com/v1";
 
-    describe('エンドポイントのパスを `.$path()` で取得できる', () => {
-      test('パスパラメータを含まない場合は、具体的なパスをそのまま返す', () => {
-        const typedHttp = createTypedHttp(petStoreApi);
-        expect(typedHttp.pets.$path()).toEqual(`${defaultBaseURL}/pets`);
-      });
+		describe("エンドポイントのパスを `.$path()` で取得できる", () => {
+			test("パスパラメータを含まない場合は、具体的なパスをそのまま返す", () => {
+				const typedHttp = createTypedHttp(petStoreApi);
+				expect(typedHttp.pets.$path()).toEqual(`${defaultBaseURL}/pets`);
+			});
 
-      test('パスパラメータ部分は `:foo` 形式で表現される', () => {
-        const typedHttp = createTypedHttp(petStoreApi);
-        expect(typedHttp.pets._id.$path()).toEqual(
-          `${defaultBaseURL}/pets/:id`,
-        );
-      });
+			test("パスパラメータ部分は `:foo` 形式で表現される", () => {
+				const typedHttp = createTypedHttp(petStoreApi);
+				expect(typedHttp.pets._id.$path()).toEqual(
+					`${defaultBaseURL}/pets/:id`,
+				);
+			});
 
-      test('`createTypedHttp` で `baseURL` オプションを指定した場合、それがベース URL として使われる', () => {
-        const typedHttp = createTypedHttp(petStoreApi, {
-          baseURL: customBaseURL,
-        });
-        expect(typedHttp.pets._id.$path()).toEqual(`${customBaseURL}/pets/:id`);
-      });
-    });
+			test("`createTypedHttp` で `baseURL` オプションを指定した場合、それがベース URL として使われる", () => {
+				const typedHttp = createTypedHttp(petStoreApi, {
+					baseURL: customBaseURL,
+				});
+				expect(typedHttp.pets._id.$path()).toEqual(`${customBaseURL}/pets/:id`);
+			});
+		});
 
-    describe('request handler を `.$get()` などで作成できる', () => {
-      const server = setupServer();
-      beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
-      afterEach(() => server.resetHandlers());
-      afterAll(() => server.close());
+		describe("request handler を `.$get()` などで作成できる", () => {
+			const server = setupServer();
+			beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+			afterEach(() => server.resetHandlers());
+			afterAll(() => server.close());
 
-      test('作成した request handler が正しく動く', async () => {
-        const typedHttp = createTypedHttp(petStoreApi);
-        server.use(
-          typedHttp.pets._id.$get(({ params }) => {
-            const petId = params.id;
-            return HttpResponse.json({ id: Number(petId), name: 'Foo' });
-          }),
-        );
+			test("作成した request handler が正しく動く", async () => {
+				const typedHttp = createTypedHttp(petStoreApi);
+				server.use(
+					typedHttp.pets._id.$get(({ params }) => {
+						const petId = params.id;
+						return HttpResponse.json({ id: Number(petId), name: "Foo" });
+					}),
+				);
 
-        const client = petStoreApi(aspida(fetch));
-        const resBody = await client.pets._id(123).$get();
+				const client = petStoreApi(aspida(fetch));
+				const resBody = await client.pets._id(123).$get();
 
-        expect(resBody).toEqual({ id: 123, name: 'Foo' });
-      });
+				expect(resBody).toEqual({ id: 123, name: "Foo" });
+			});
 
-      test('aspida で指定された型以外のレスポンスボディを返したい場合は、型パラメータでそれを表現できる', async () => {
-        const typedHttp = createTypedHttp(petStoreApi);
-        type ErrorResponseBody = { errorCode: string };
-        server.use(
-          typedHttp.pets._id.$get<ErrorResponseBody>(({ params }) => {
-            const petId = params.id;
-            if (petId === 'bad_id') {
-              return HttpResponse.json(
-                { errorCode: 'resource_not_found' },
-                { status: 404 },
-              );
-            }
-            return HttpResponse.json({ id: Number(petId), name: 'Foo' });
-          }),
-        );
+			test("aspida で指定された型以外のレスポンスボディを返したい場合は、型パラメータでそれを表現できる", async () => {
+				const typedHttp = createTypedHttp(petStoreApi);
+				type ErrorResponseBody = { errorCode: string };
+				server.use(
+					typedHttp.pets._id.$get<ErrorResponseBody>(({ params }) => {
+						const petId = params.id;
+						if (petId === "bad_id") {
+							return HttpResponse.json(
+								{ errorCode: "resource_not_found" },
+								{ status: 404 },
+							);
+						}
+						return HttpResponse.json({ id: Number(petId), name: "Foo" });
+					}),
+				);
 
-        const client = petStoreApi(aspida(fetch));
-        const resBody = await client.pets._id(123).$get();
+				const client = petStoreApi(aspida(fetch));
+				const resBody = await client.pets._id(123).$get();
 
-        expect(resBody).toEqual({ id: 123, name: 'Foo' });
-      });
+				expect(resBody).toEqual({ id: 123, name: "Foo" });
+			});
 
-      test('`createTypedHttp` で `baseURL` オプションを指定した場合、それがベース URL として使われる', async () => {
-        const typedHttp = createTypedHttp(petStoreApi, {
-          baseURL: customBaseURL,
-        });
+			test("`createTypedHttp` で `baseURL` オプションを指定した場合、それがベース URL として使われる", async () => {
+				const typedHttp = createTypedHttp(petStoreApi, {
+					baseURL: customBaseURL,
+				});
 
-        server.use(
-          typedHttp.pets._id.$get(({ params }) => {
-            const petId = params.id;
-            return HttpResponse.json({ id: Number(petId), name: 'Foo' });
-          }),
-        );
+				server.use(
+					typedHttp.pets._id.$get(({ params }) => {
+						const petId = params.id;
+						return HttpResponse.json({ id: Number(petId), name: "Foo" });
+					}),
+				);
 
-        const client = petStoreApi(aspida(fetch, { baseURL: customBaseURL }));
-        const resBody = await client.pets._id(123).$get();
-        expect(resBody).toEqual({ id: 123, name: 'Foo' });
-      });
-    });
+				const client = petStoreApi(aspida(fetch, { baseURL: customBaseURL }));
+				const resBody = await client.pets._id(123).$get();
+				expect(resBody).toEqual({ id: 123, name: "Foo" });
+			});
+		});
 
-    // TODO: 型のテストを追加する
-  });
+		// TODO: 型のテストを追加する
+	});
 });
